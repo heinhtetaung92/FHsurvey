0a4ba9b1119d75815ccad08ee918862834c579f0
heinhtet <heinhtetaunghtetaung92@gmail.com>
2015-07-02 17:59:11 +0630

    with scrollview

--- app/src/main/java/com/algo/hha/fhsurvey/QuestionFormActivity.java
+++ app/src/main/java/com/algo/hha/fhsurvey/QuestionFormActivity.java
@@ -1,18 +1,28 @@
 package com.algo.hha.fhsurvey;
 
+import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.algo.hha.fhsurvey.adapter.QuestionFormAdapter;
 import com.algo.hha.fhsurvey.api.RetrofitAPI;
+import com.algo.hha.fhsurvey.configuration.AnswerType;
 import com.algo.hha.fhsurvey.model.QuestionFormData;
 
 import org.json.JSONArray;
@@ -32,20 +42,16 @@ import retrofit.client.Response;
  */
 public class QuestionFormActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
 
-    ListView question_listview;
+    ScrollView question_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_form);
 
-        question_listview = (ListView) findViewById(R.id.question_form_listview);
 
         List<List<QuestionFormData>> datalist = new ArrayList<>();
-
-        QuestionFormAdapter adp = new QuestionFormAdapter(this, datalist);
-        question_listview.setAdapter(adp);
-        question_listview.setOnItemClickListener(this);
+        question_view = (ScrollView) findViewById(R.id.question_form_scrollview);
 
         Bundle bundle = getIntent().getExtras();
         if(bundle != null){
@@ -68,7 +74,7 @@ public class QuestionFormActivity extends ActionBarActivity implements AdapterVi
             @Override
             public void success(String s, Response response) {
                 List<List<QuestionFormData>> datalist = parseJSONToObject(s);
-                addDataToListView(datalist);
+                addDataToScrollView(datalist);
             }
 
             @Override
@@ -222,11 +228,150 @@ public class QuestionFormActivity extends ActionBarActivity implements AdapterVi
 
 
 
-    private void addDataToListView(List<List<QuestionFormData>> dl){
-        if(question_listview == null) return;
+    private void addDataToScrollView(List<List<QuestionFormData>> dl){
+        if(question_view == null) return;
 
-        QuestionFormAdapter adp = (QuestionFormAdapter) question_listview.getAdapter();
-        adp.add(dl);
+        question_view.removeAllViews();
+
+
+        //crate linearlayout as main layout
+        LinearLayout linearLayout = new LinearLayout(this);
+        //set layout param of abslistview
+        ScrollView.LayoutParams param = new ScrollView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        linearLayout.setLayoutParams(param);
+        //set orientation
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        for(int k=0;k<dl.size();k++){
+            View childView = matchUIwithItemType(k, dl);
+            if(childView != null)
+                linearLayout.addView(childView);
+
+        }
+
+        linearLayout.addView(createSaveButton());
+
+
+        question_view.addView(linearLayout);
+
+    }
+
+    private View createSaveButton(){
+        Button btnSave = new Button(this);
+        btnSave.setText("Save");
+        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        btnSave.setLayoutParams(param);
+        btnSave.setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+
+        return btnSave;
+    }
+
+    private View matchUIwithItemType(int position, List<List<QuestionFormData>> dl){
+        List<QuestionFormData> itemlist = dl.get(position);
+        if(itemlist.size() <= 0){
+            return null;
+        }else{
+            String answerTypeDesc = itemlist.get(0).get_AnswerTypeDescription();
+            if(answerTypeDesc.equals(AnswerType.SINGLE_CHOICE)){//its type is radio button
+                return createSingleChoiceAnswer(position, dl);
+            }else if(answerTypeDesc.equals(AnswerType.TEXT)){//its type is Text type
+                return createTextInputAnswer(position, dl);
+            }
+        }
+        return null;
+    }
+
+    /***
+     * this method create a view with radio button data from server
+     * @param position
+     * list's position to get data
+     * @return view
+     * to use as listview's item
+     */
+    private View createSingleChoiceAnswer(int position, List<List<QuestionFormData>> dl){
+        List<QuestionFormData> itemlist = dl.get(position);
+
+        //crate linearlayout as main layout
+        LinearLayout linearLayout = new LinearLayout(this);
+        //set layout param of abslistview
+        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        linearLayout.setLayoutParams(param);
+        //set orientation
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        TextView tv_answer_title = new TextView(this);
+        LinearLayout.LayoutParams title_param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        title_param.setMargins(8, 8, 8, 8);
+        tv_answer_title.setLayoutParams(title_param);
+
+        tv_answer_title.setText(itemlist.get(0).get_AnswerDescription());
+        tv_answer_title.setTextSize(18);
+        tv_answer_title.setTypeface(null, Typeface.BOLD);
+        linearLayout.addView(tv_answer_title);
+
+        //create radio group for all radio buttons
+        RadioGroup rd_group = new RadioGroup(this);
+        LinearLayout.LayoutParams rd_group_param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        rd_group_param.setMargins(8, 8, 8, 8);
+        rd_group.setLayoutParams(rd_group_param);
+
+        //this loop create radio button per data count
+        for(int i=0;i<itemlist.size();i++){
+            RadioButton radioButton = new RadioButton(this);
+            radioButton.setText(itemlist.get(i).get_AnswerDescription());
+            rd_group.addView(radioButton);
+
+            //checked first item(default)
+            if(i == 0)
+                radioButton.setChecked(true);
+        }
+        linearLayout.addView(rd_group);
+
+        return linearLayout;
+    }
+
+    /***
+     * this method create EditText with title
+     * @param position
+     * list's position to get data
+     * @return view
+     * to use as listview's item
+     */
+
+    private View createTextInputAnswer(final int position, List<List<QuestionFormData>> dl){
+
+        List<QuestionFormData> itemlist = dl.get(position);
+
+        //crate linearlayout as main layout
+        LinearLayout linearLayout = new LinearLayout(this);
+        //set layout param of abslistview
+        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        linearLayout.setLayoutParams(param);
+        //set orientation
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        //create title(Question) for edittext
+        TextView tv_answer_title = new TextView(this);
+        LinearLayout.LayoutParams title_param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        title_param.setMargins(8, 8, 8, 8);
+        tv_answer_title.setLayoutParams(title_param);
+
+        tv_answer_title.setText(itemlist.get(0).get_QuestionDescription());
+        tv_answer_title.setTextSize(18);
+        tv_answer_title.setTypeface(null, Typeface.BOLD);
+        linearLayout.addView(tv_answer_title);
+
+        //create edittext as requirement
+        final EditText editText = new EditText(this);
+        LinearLayout.LayoutParams editText_param = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        title_param.setMargins(8, 8, 8, 8);
+        editText.setSingleLine();
+        editText.setLayoutParams(editText_param);
+        editText.setEms(10);
+
+        linearLayout.addView(editText);
+
+        return linearLayout;
     }
 
 }
